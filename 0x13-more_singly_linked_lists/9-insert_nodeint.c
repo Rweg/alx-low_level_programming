755c5b32ccff8a09dda550ba284368d3d2c83b9d
RWEGO Toussaint <toussaint.rwego@flyzipline.com>
2023-04-30 15:13:03 +0200

    Fix v4  task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,18 +14,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	temp = *head;
@@ -34,12 +34,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (temp == NULL || temp->next == NULL)
 		{
 			free(new_node);
-			return NULL;
+			return (NULL);
 		}
 		temp = temp->next;
 	}
 
 	new_node->next = temp->next;
 	temp->next = new_node;
-	return new_node;
+	return (new_node);
 }
