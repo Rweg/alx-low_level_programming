fb0518591ea2986868ce3bd308b88b37ba785161
RWEGO Toussaint <toussaint.rwego@flyzipline.com>
2023-05-03 03:04:08 +0200

    103-find_loop.c Fix

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,29 +11,24 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
-	int flag = 0;
+
+	if (!head)
+		return (NULL);
 
 	while (slow && fast && fast->next)
 	{
-		slow = slow->next;
 		fast = fast->next->next;
-
-		if (slow == fast)
+		slow = slow->next;
+		if (fast == slow)
 		{
-			flag = 1;
-			break;
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
 		}
-	}
-
-	if (flag)
-	{
-		slow = head;
-		while (slow != fast)
-		{
-			slow = slow->next;
-			fast = fast->next;
-		}
-		return (fast);
 	}
 
 	return (NULL);
