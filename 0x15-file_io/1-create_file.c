f6b597494c382d500408a05ab4ab7e87e1d95a49
Toussaint Rwego <toussaint.rwego@flyzipline.com>
2023-07-11 23:17:21 +0200

    Fix 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,34 +8,35 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-        int fd, w, len = 0;
+	int fd, w, len = 0;
 
-        if (!filename)
-                return (-1);
+	if (!filename)
+		return (-1);
 
-        if (text_content)
-        {
-                while (text_content[len])
-                        len++;
-        }
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-        fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-        if (fd == -1)
-                return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-        if (text_content)
-        {
-                w = write(fd, text_content, len);
-                if (w == -1 || w != len)
-                {
-                        close(fd);
-                        return (-1);
-                }
-        }
+	if (text_content)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1 || w != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-        close(fd);
+	close(fd);
 
-        return (1);
+	return (1);
 }
