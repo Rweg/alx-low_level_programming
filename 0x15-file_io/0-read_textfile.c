f096c56025e726f8b9b36611e781b9ddd0fcbaae
Toussaint Rwego <toussaint.rwego@flyzipline.com>
2023-07-11 23:10:50 +0200

    Fix 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: Pointer to the name of the file to read
- * @letters: The number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
  *
- * Return: The number of letters it can read and print, or 0 if an error occurs
+ * Return: the actual number of letters read and printed, or 0 on failure
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd;
-        ssize_t txtread, txtwritten;
-        char *buffer;
+	int fd;
+	char *buf;
+	ssize_t bytes_read, bytes_written;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(sizeof(char) * (letters + 1));
-        if (buffer == NULL)
-                return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-        {
-                free(buffer);
-                return (0);
-        }
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-        txtread = read(fd, buffer, letters);
-        if (txtread == -1)
-        {
-                free(buffer);
-                close(fd);
-                return (0);
-        }
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 
-        buffer[txtread] = '\0';
-        txtwritten = write(STDOUT_FILENO, buffer, txtread);
-        if (txtwritten == -1 || txtwritten != txtread)
-        {
-                free(buffer);
-                close(fd);
-                return (0);
-        }
+	buf[bytes_read] = '\0';
 
-        free(buffer);
-        close(fd);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 
-        return (txtwritten);
+	close(fd);
+	free(buf);
+
+	return (bytes_written);
 }
