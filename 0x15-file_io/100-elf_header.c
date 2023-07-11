2e79813aecb10d57b7a26d5359d8ed81ca1427d9
Toussaint Rwego <toussaint.rwego@flyzipline.com>
2023-07-11 23:49:58 +0200

    Fix 7

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,22 @@
-#include <elf_header.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void vldt_e(unsigned char *e_ident);
+void p_magic(unsigned char *e_ident);
+void p_class(unsigned char *e_ident);
+void p_data(unsigned char *e_ident);
+void p_version(unsigned char *e_ident);
+void p_abi(unsigned char *e_ident);
+void p_osabi(unsigned char *e_ident);
+void p_type(unsigned int e_type, unsigned char *e_ident);
+void p_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_e(int elf);
 
 /**
  * vldt_e - It verifies if a file is an ELF file.
