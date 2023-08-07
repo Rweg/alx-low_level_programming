a1dd1851b721cb53d82496c51bb6e25d07896cfb
Toussaint Rwego <toussaint.rwego@flyzipline.com>
2023-08-07 23:11:07 +0200

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int src, dest, r, w;
+	char buffer[1024];
+	char *error_msg = "Error: Can't read from file";
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	src = open(argv[1], O_RDONLY), r = read(src, buffer, 1024);
+	if (src == -1 || r == -1)
+		dprintf(STDERR_FILENO, error_msg, argv[1]), exit(98);
+
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	while (r > 0)
+	{
+		w = write(dest, buffer, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+		r = read(src, buffer, 1024);
+	}
+
+	close(src), close(dest);
+	return (0);
+}
